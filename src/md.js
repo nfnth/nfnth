9e4f561c097eac4209953875a4976db4620c3284
M <86447066+nfnth@users.noreply.github.com>
2021-08-12 10:10:07 -0700

    Update md.js

--- src/md.js
+++ src/md.js
@@ -61,4 +61,4 @@ function camera() {
 var video = document.getElementById('video');
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) { // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {//video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream; video.play(); }); }
+        video.srcObject = stream; video.play(); }); } }
