e6fcf21c2a603e7f4470d3fa5ab807602e651b05
M <86447066+nfnth@users.noreply.github.com>
2021-08-11 23:42:43 -0700

    Update site.py

--- src/site.py
+++ src/site.py
@@ -40,6 +40,53 @@ async def stream(request):
     #...for latest feed
     return web.FileResponse(PATH + '/ur.js') #make "artifact" specific
 
+import threading, email, uuid, smtplib, base64, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from aiosmtpd.controller import Controller
+	
+##(server)
+class MailHandler():
+    async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
+        if not address.endswith('@ur.land'): #use variable...
+            return '550 not relaying to that domain'
+        envelope.rcpt_tos.append(address)
+        return '250 OK'
+
+    async def handle_DATA(self, server, session, envelope): #check len(data) just in case...
+        # filter project@user.servius.me
+        #print('Message from %s' % envelope.mail_from)
+        #print('Message for %s' % envelope.rcpt_tos)
+        #print('Message data:\n')
+        #message = email.message_from_string(envelope.content.decode('utf8', errors='replace'))
+        mail_id = str(uuid.uuid4())
+
+        if os.path.exists(REL + '/data/mail/' + str(envelope.rcpt_tos)):
+            with open(REL + '/data/mail/' + str(envelope.rcpt_tos) + '/' + mail_id, 'w') as f:
+                f.write(envelope.content.decode('utf8', errors='replace'))
+
+        return '250 Message accepted for delivery'
+
+def handleAttachment():
+    counter = 1
+    for part in msg.walk():
+        if part.get_content_maintype() == 'multipart': # multipart/* are just containers
+            continue
+        filename = part.get_filename() # Applications should really sanitize the given filename so that an email message can't be used to overwrite important files
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                ext = '.bin'
+            filename = 'part-%03d%s' % (counter, ext)
+        counter += 1
+        fp = open(os.path.join(opts.directory, filename), 'wb')
+        fp.write(part.get_payload(decode=True))
+        fp.close()
+
+async def mail(port):
+    controller = Controller(MailHandler(), hostname='localhost', port=5000+port)
+    controller.start()	
+
 # server
 import asyncio, aiohttp
 from aiohttp import web
