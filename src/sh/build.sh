9a688e32f301bf96ae1b2f1e5241d7cf4eb38ae6
M <86447066+nfnth@users.noreply.github.com>
2021-08-18 11:49:51 -0700

    Update build.sh

--- src/sh/build.sh
+++ src/sh/build.sh
@@ -1,49 +1,4 @@
 #!/bin/bash
-#live feed/file attachment/user profile back to index...
-#add image maker ISO download...
- #ip link set $(echo $(ls -d /sys/class/net/w*) | sed 's/\/sys\/class\/net\///g') down
-#systemctl set-default graphical.target
-
-#ln -sT /usr/share/zoneinfo/${timezone} /etc/localtime # -sf ?
-    #timedatectl set-timezone ${timezone}
-    #timedatectl set-ntp true 
-    #    cat > /etc/systemd/system/getty@tty1.service.d/skip-prompt.conf <<EOF
-#[Service]
-#ExecStart=
-#ExecStart=-/usr/bin/agetty --skip-login --nonewline --noissue --autologin username --noclear %I $TERM
-#EOF
-    #boot menu still shows?
-    #amixer sset Headphone unmute
-
-    #cat /boot/loader/loader.conf ...set timeout to 0...
-#Run systemctl edit getty@tty1 and replace the contents with:
-
-#[Service]
-#ExecStart=
-#ExecStart=-/usr/bin/agetty --skip-login --login-options "-f usernamehere" %I 38400 linux
-
-#xenv="env DISPLAY=:0 XAUTHORITY=/home/my_name/.Xauthority"
-    # button map before
-    #$xenv /usr/bin/xinput
-
-    #pacman-key --init
-    #pacman-key --populate archlinux # pacman -S archlinux-keyring for bad keys...
-
-    # fbida ipw2100-fw ipw2200-fw dnsmasq mozilla-common
-    # gptfdisk
-    #  noto-fonts noto-fonts-extra noto-fonts-emoji
-    #minimize above packages...?
-    # not used anymore... firefox intel-ucode base base-devel amd-ucode memtest86+ tor vi vim-minimal 
-
-    #systemctl enable org.cups.cupsd.service #https://localhost:631, user: root, password: root password, use lp or lpr?
-    #cnijfilter-ip110 ... from aur.archlinux.org
-    #cups cups-filters ghostscript
-
-lsof -i -P -n        
-        xset -dpms; xset s off &https://wiki.archlinux.org/index.php/DPMS
-        
-        env MOZ_USE_XINPUT2=1 firefox
-...about settings? 
 
 source="on" #on|off
 boot="disk" #iso|disk
@@ -68,19 +23,12 @@ client="linux linux-firmware syslinux mkinitcpio edk2-shell efibootmgr gptfdisk 
 #bluez bluez-utils, enable bluetooth service, use bluetoothctl to scan/list/pair/connect
 #vulkan?
 
-server="haproxy certbot python python-pip git"
-python="aiohttp asyncio aiosmtpd" #av aiortc opencv-python
-##object-mapper webkit pywebview pyotp tensorflow pytorch aiohttp_session numpy-stl
-
 timezone=""
 host="bros"
 
 wifi_adapter=$(echo $(ls -d /sys/class/net/w*) | sed 's/\/sys\/class\/net\///g')
 wifi_id=""
 wifi_password=""
-
-client_ip="192.168.1.126"
-mode="setup" #setup|deploy
 
 base() {
     parted -s ${drive} mklabel gpt mkpart primary fat32 1 300M mkpart primary ext2 300M 100% set 1 esp on
@@ -160,16 +108,6 @@ install() {
 #EOF
     iwctl --passphrase ${wifi_password} station ${wifi_adapter} connect ${wifi_id} #optional, wifi
 
-    cat > /usr/bin/bros <<EOF #/etc/X11/?/xinitrc
-EOF
-    
-    cat > /etc/systemd/system/bros.service <<EOF
-
-EOF
-    
-    chmod +x /usr/bin/bros
-    systemctl enable bros.service
-
     boot
 }
 
@@ -178,22 +116,14 @@ boot() {
     
     mv /boot/loader/entries/archiso-x86_64-linux.conf /boot/loader/entries/archiso-x86_64-linux.conf.old
     mv /boot/syslinux/syslinux.cfg /boot/syslinux/syslinux.cfg.old
-
-    cat > /boot/loader/entries/archiso-x86_64-linux.conf <<EOF
-EOF
-
-    cat > /boot/syslinux/syslinux.cfg <<EOF
-
-EOF
-
-    sed -i "s/root=XXXX/root=${partuuid}/g" /boot/loader/entries/archiso-x86_64-linux.conf
-    sed -i "s/root=XXXX/root=${partuuid}/g" /boot/syslinux/syslinux.cfg
-
     mv /etc/mkinitcpio.conf /etc/mkinitcpio.conf.old
     
-    cat > /etc/mkinitcpio.conf <<EOF
-
-EOF
+    cp linux.conf /boot/loader/entries/archiso-x86_64-linux.conf
+    cp syslinux.cfg /boot/syslinux/syslinux.cfg
+    cp mkinitcpio.conf /etc/mkinitcpio.conf
+    
+    sed -i "s/root=XXXX/root=${partuuid}/g" /boot/loader/entries/archiso-x86_64-linux.conf
+    sed -i "s/root=XXXX/root=${partuuid}/g" /boot/syslinux/syslinux.cfg
 
     mkinitcpio -P
     syslinux-install_update -i -a -m
@@ -202,9 +132,53 @@ EOF
 if [[ "$1" == "install" ]]
 then
     install
-elif [[ "$1" == "deploy" ]]
-then
-    deploy
 else
     base
 fi
+
+#live feed/file attachment/user profile back to index...
+#add image maker ISO download...
+ #ip link set $(echo $(ls -d /sys/class/net/w*) | sed 's/\/sys\/class\/net\///g') down
+#systemctl set-default graphical.target
+
+#ln -sT /usr/share/zoneinfo/${timezone} /etc/localtime # -sf ?
+    #timedatectl set-timezone ${timezone}
+    #timedatectl set-ntp true 
+    #    cat > /etc/systemd/system/getty@tty1.service.d/skip-prompt.conf <<EOF
+#[Service]
+#ExecStart=
+#ExecStart=-/usr/bin/agetty --skip-login --nonewline --noissue --autologin username --noclear %I $TERM
+#ExecStart=-/usr/bin/agetty --skip-login --login-options "-f usernamehere" %I 38400 linux
+#EOF
+    #boot menu still shows?
+    #amixer sset Headphone unmute
+
+    #cat /boot/loader/loader.conf ...set timeout to 0...
+#Run systemctl edit getty@tty1 and replace the contents with:
+
+#xenv="env DISPLAY=:0 XAUTHORITY=/home/my_name/.Xauthority"
+    # button map before
+    #$xenv /usr/bin/xinput
+
+    #pacman-key --init
+    #pacman-key --populate archlinux # pacman -S archlinux-keyring for bad keys...
+
+    # fbida ipw2100-fw ipw2200-fw dnsmasq mozilla-common
+    # gptfdisk
+    #  noto-fonts noto-fonts-extra noto-fonts-emoji
+    #minimize above packages...?
+    # not used anymore... firefox intel-ucode base base-devel amd-ucode memtest86+ tor vi vim-minimal 
+
+    #systemctl enable org.cups.cupsd.service #https://localhost:631, user: root, password: root password, use lp or lpr?
+    #cnijfilter-ip110 ... from aur.archlinux.org
+    #cups cups-filters ghostscript
+
+#lsof -i -P -n        
+ #       xset -dpms; xset s off &https://wiki.archlinux.org/index.php/DPMS
+        
+#        env MOZ_USE_XINPUT2=1 firefox
+#...about settings? 
+
+    chmod +x /usr/bin/bros
+    systemctl enable bros.service
+    #nohup python /root/nfnth/run.py > /dev/null 2>&1 & disown & #exec?
