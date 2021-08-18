b900cf1c0bb8d3456e8d5ca86573f8a9c912d425
M <86447066+nfnth@users.noreply.github.com>
2021-08-17 21:36:09 -0700

    Update md.js

--- src/js/md.js
+++ src/js/md.js
@@ -1,9 +1,9 @@
 
 //"artifact editor", - [RegExp online](https://regexr.com/)
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.2.0/fabric.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+//	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.2.0/fabric.min.js"></script>
+//<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+//<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 var poly = "https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch";
 $.getScript(poly, function() {  });
