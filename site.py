07b289c822be427cf81403de0f7c20c0af27f36e
M <o@ocur.io>
2022-04-08 11:45:13 -0700

    Update site.py

--- site.py
+++ site.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
-import os, socket, json, types, shutil#, six
+import os, socket, json, types#, shutil#, six
 import requests, random
-#from os import walk
-#from datetime import datetime
+
 from web3.auto import w3
 from web3 import HTTPProvider, Web3
 from distutils.dir_util import copy_tree
-import subprocess
+#import subprocess
 import uuid
-import shlex
+#import shlex
 from hexbytes import HexBytes
 #from web3 import Web3
 #from hexbytes import HexBytes
@@ -29,26 +28,6 @@ REL=os.getcwd()
 async def index(request): #request.remote_addr #if not request.host == "dralun.com" and not request.host == "ur.land":
     return web.FileResponse(PATH + '/res/index.htm')
 
-async def path(request):
-    item = request.match_info.get('item', 'domain') #wallet
-#    domain = request.match_info.get('domain', 'tactician.us')
-    location = DATA + item
-
-#    if not os.path.exists(location):
-#        os.mkdir(location)
-        #src = PATH + '/data/domain/ur.land'
-        #shutil.copytree(src, location)
-        #copy_tree(src, location)
-
-    tree = []
-    for (root, dirs, files) in os.walk(location):
-        for d in dirs:
-            tree.append(os.path.join(root, d)) #extend?
-        #for f in files:
-            #tree.append(os.path.join(root, f))
-
-    return web.Response(text='|'.join(tree),content_type="text/html")
-
 async def key(request): #secret hash?
     hostname = socket.gethostname()
 ## getting the IP address using socket.gethostbyname() method
@@ -67,8 +46,7 @@ async def api(request):
 
 async def data(request):
     wallet = request.match_info.get('wallet', '')
-    item = request.match_info.get('item', 'wallet') #domain
-    action = request.match_info.get('action', 'add') #edit delete
+    domain = request.match_info.get('tactician.us', '')
 
     content = "No profile listed."
     async for field in (await request.multipart()):
@@ -107,24 +85,19 @@ async def data(request):
     message_wallet = Account.recover_message(encoded, signature=signature)
     print (message_wallet)
 
-
     if wallet.upper() == message_wallet.upper(): #user authenticated
         print ("authenticated...")
         owner = False
 
-        if item == "wallet":
-            location = DATA + '/wallet/' + wallet.upper()
-            owner = True
-        elif item == "domain":
-            #domain = request.host
-            location = PATH + '/domain/' + domain
+        #domain = request.host
+        location = PATH + '/domain/' + domain
 
-            api_url = "https://api.opensea.io/api/v1/asset/0x495f947276749Ce646f68AC8c248420045cb7b5e/" + ref
-            print (api_url)
-            x = requests.get(api_url).json().get("top_ownerships")[0].get("owner").get("address")
-            print (x)
-            if x.upper() == wallet.upper():
-                owner = True
+        api_url = "https://api.opensea.io/api/v1/asset/0x495f947276749Ce646f68AC8c248420045cb7b5e/" + ref
+        print (api_url)
+        x = requests.get(api_url).json().get("top_ownerships")[0].get("owner").get("address")
+        print (x)
+        if x.upper() == wallet.upper():
+            owner = True
             #alltransactions = x.json().get("assets")
             #for t in alltransactions:
             #    name = t.get("name")
@@ -186,21 +159,14 @@ async def site(port):
     app = web.Application(client_max_size=10000000)
 
     app.router.add_static('/res', PATH + '/res')
-    app.router.add_static('/doc', PATH + '/data')
-
-    if os.path.isdir('/mnt/img/img'):
-        app.router.add_static('/img', '/mnt/img/img')
-    if os.path.isdir('/mnt/deed'):
-        app.router.add_static('/deed', '/mnt/deed')
+    app.router.add_static('/img', '/mnt/img/img')
+    app.router.add_static('/domain', PATH + '/domain')
 
     app.add_routes([web.get('/', index)])
+    app.add_routes([web.get('/api/{wallet}', api)])
     app.add_routes([web.get('/key', key)]) #auth/anti-bot
-    app.add_routes([web.get('/api/{account}', api)])
 
-    app.add_routes([web.get('/path/{item}', path)])
-    app.add_routes([web.post('/data/{wallet}/{item}/{action}', data)])
-    app.add_routes([web.get('/{deed}', index)])
-    app.add_routes([web.get('/{deed}/{domain}', index)])
+    app.add_routes([web.post('/data/{wallet}/{domain}', data)])
     
     runner = web.AppRunner(app)
     loop = asyncio.new_event_loop()
