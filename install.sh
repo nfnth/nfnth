1785a6eb17cf955614726231f368b781a733e049
M <o@ocur.io>
2022-04-11 13:54:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,30 @@ server() {
 domain() {
     openssl ecparam -genkey -name secp384r1 | openssl ec -out ecc-privkey.pem
     cp openssl.cnf /etc/ssl/openssl.cnf
+    
+    #COUNTER=0
+    #COMMAND=""
+    #while IFS= read -r line;do
+    #    fields=($(printf "%s" "$line"|cut -d'|' --output-delimiter=' ' -f1-))
+    #    let COUNTER++
+    #    echo "DNS."$COUNTER" = "${fields[0]} >> /etc/ssl/openssl.cnf
+    #    COMMAND="${COMMAND} -d ${fields[0]}"
+
+    #    if (( $COUNTER % 100 == 0 )); then
+    #        echo $COMMAND
+    #        openssl req -new -sha256 -key ecc-privkey.pem -nodes -outform pem -out ecc-csr.pem -subj /C=US/ST=Washington/L=Seattle/O=UR.Land/OU=House/CN=${fields[0]}
+    #        certbot certonly ${COMMAND} --email matt@sebolt.us --csr ecc-csr.pem --agree-tos --non-interactive --standalone
+    #        sudo -E bash -c 'cat 0000_cert.pem >> alldomains.pem'
+    #        COMMAND=""
+    #        rm 0000_cert.pem
+    #        rm 0000_chain.pem
+    #        cp /root/nfnth/res/sh/config/openssl.cnf /etc/ssl/openssl.cnf
+
+    #        echo "Continue?"
+    #        read input < /dev/tty
+    #    fi
+    #done < /etc/haproxy/domain
+    
     echo "DNS.01 = "${domain} >> /etc/ssl/openssl.cnf
     openssl req -new -sha256 -key ecc-privkey.pem -nodes -outform pem -out ecc-csr.pem -subj /C=US/ST=Washington/L=Seattle/O=ocur.io/OU=House/CN=${domain} #${fields[0]}
     certbot certonly -d ${domain} --email matt@sebolt.us --csr ecc-csr.pem --agree-tos --non-interactive --standalone
