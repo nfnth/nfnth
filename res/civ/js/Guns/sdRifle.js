49b8364138a168a69f947ec24555c95d71a8671e
m <o@ocur.io>
2022-05-13 12:18:31 -0700

    adjustment

--- res/civ/js/Guns/sdRifle.js
+++
@@ -1,3 +0,0 @@
-class sdRifle extends sdGun
-{
-}
--- res/civ/js/Guns/sdSniper.js
+++
@@ -1,9 +0,0 @@
-class sdSniper extends sdGun
-{
-	change_transformations( c, third=false )
-	{
-		const sniper_reload_offset = -5;
-		if ( c.reload_timer + sniper_reload_offset > 0 && c.reload_timer + sniper_reload_offset < Math.PI * 2 )
-		c.cur_weapon_mesh.position.z += Math.sin( c.reload_timer + sniper_reload_offset ) * 0.2;
-	}
-}
