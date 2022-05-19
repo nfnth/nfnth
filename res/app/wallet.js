5e579287d30198a806494ec1426a01b093f456f0
M <m@nfnth.com>
2022-05-19 13:22:14 -0700

    Update wallet.js

--- res/app/wallet.js
+++ res/app/wallet.js
@@ -22,9 +22,13 @@ function desetup() { $("#wallet-area").addClass("blue-grey"); $("#wallet-area").
     emptyDeeds(); domainSelect(); }
 
 function listDeeds() {
-	for (let j = 0; j < domains.length; j++) { if (domains[j].core.token_id == user.deed[i].id) {  user.deed[i].name = domains[j].core.name; j = domains.length; } }
+	
     $('#domain-template').empty(); $('#domain-template').append('<option selected="selected">No domain selected</option>');
-    for (let i = 0; i < user.deed.length; i++) { $('#domain-template').append('<option selected="selected" value="' + user.deed[i].id + '">' + user.deed[i].name + '</option>'); }
+    for (let i = 0; i < user.deed.length; i++) { 
+	    
+	    for (let j = 0; j < domains.length; j++) { if (domains[j].core.token_id == user.deed[i].id) {  user.deed[i].name = domains[j].core.name; j = domains.length; } }
+	    
+	    $('#domain-template').append('<option selected="selected" value="' + user.deed[i].id + '">' + user.deed[i].name + '</option>'); }
     document.getElementById('domain-template').getElementsByTagName('option')[0].selected = 'selected';
     $("#domain-template").formSelect(); $('select').formSelect(); }
 
