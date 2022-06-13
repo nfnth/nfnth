379cc66a259bcf90b6d0c9681997a05c124ab9dc
M <m@nfnth.com>
2022-06-13 13:38:37 -0700

    Update wallet.js

--- res/app/wallet.js
+++ res/app/wallet.js
@@ -59,7 +59,7 @@ function pullDomain(domain) { domainMd = ""; domainMd = new Md();
 
 function buildDomain() {
 	for (let i = 1; i < artifacts.length; i++) { 
-		$("#registry-artifact").html(); $("#registry-artifact").append("<a class='collection-item'>" + artifacts[i].name + "			   }  
+		$("#registry-artifact").html(); $("#registry-artifact").append("<a class='collection-item'>" + artifacts[i].name + "</a>");			   }  
 }
 
 function postDomain() {
