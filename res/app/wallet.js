02ef843b00bd058b11f4ad8ca5c06f5a9cf07914
M <m@nfnth.com>
2022-06-17 16:04:55 -0700

    Update wallet.js

--- res/app/wallet.js
+++ res/app/wallet.js
@@ -40,9 +40,9 @@ function emptyDeeds() { $('#domain-template').empty();
 function domainSelect() { 
 	var d = document.getElementById("domain-template"); var dt = d.options[d.selectedIndex].text; var dv = d.options[d.selectedIndex].value; 
     	if (dt == 'No domain selected') { $("#poster").addClass("disabled"); $("#trader").addClass("disabled");$("#builder").addClass("disabled"); 
-		$("#myDomain").html('My Domain Balance'); $("#myArt").html('My Domain Artifact'); }
+		$("#myDomain").html('My Domain Balance'); $("#myDomain").removeClass("badge"); $("#myDomain").removeClass("amber"); $("#myArt").html('My Domain Artifact'); }
     	else { $("#poster").removeClass("disabled");  $("#trader").removeClass("disabled"); $("#builder").removeClass("disabled");
-	     $("#myDomain").html('24,901 UR'); $("#myArt").html('9 artifact');} pullDomain(dv);  }
+	     $("#myDomain").html('24,901 UR'); $("#myDomain").addClass("badge"); $("#myDomain").addClass("amber"); $("#myArt").html('9 artifact');} pullDomain(dv);  }
 
 function pullDomain(domain) { domainMd = ""; domainMd = new Md(); 
 	$("#selink").removeClass("disabled"); $("#dropdown-deeder").removeClass("disabled"); $("#swilink").removeClass("disabled");
