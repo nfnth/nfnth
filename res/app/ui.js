fe128aea065e9fcd67f440aed827de40b9e58f48
M <m@nfnth.com>
2022-09-06 12:51:52 -0700

    Update ui.js

--- res/app/ui.js
+++ res/app/ui.js
@@ -1,4 +1,4 @@
-var complete; var reset = false; var first = true; var nfnth = true;
+var complete; var reset = false; var first = true; var nfnth = false;
 function showNfNth() { if (nfnth) {nfnth = false;} else {nfnth = true;}  }
 function showList(area) {  $("#registry").html(''); $("#registry").hide(); 
     switch (area) {
