a65c509a5b3e5abc836a34d352c86fc330da7322
M <m@nfnth.com>
2022-09-08 08:27:35 -0700

    Update map.js

--- res/app/map.js
+++ res/app/map.js
@@ -144,36 +144,9 @@ function showEdit() { if (tempMark != "") { tempMark.remove(); tempMark = ""; }
     	//style += ";background-image:url('img/icon/domain/"+folder + "/" +icon+".png'); 
 		     
 	
-	tempMark.setPopup(new AnimatedPopup({ offset: 25, openingAnimation: {duration: 1000, easing: 'easeOutElastic'}, closingAnimation: { duration: 200, easing: 'easeInBack' } }).setHTML(formCode));
-	
-	
-	//add '.ready' to form when user focuses on it
-$("#email").focus(function(){
-	$("#cuboid form").addClass("ready");
-})
-//remove '.ready' when user blus away but only if there is no content
-$("#email").blur(function(){
-	if($(this).val() == "")
-		$("#cuboid form").removeClass("ready");
-})
-
-//If the user is typing something make the arrow green/.active
-$("#email").keyup(function(){
-	//this adds .active class only if the input has some text
-	$(".submit-icon").toggleClass("active", $(this).val().length > 0);
-})
-
-//on form submit remove .ready and add .loading to the form
-$("#cuboid form").submit(function(){
-	$(this).removeClass("ready").addClass("loading");
-	//finish loading in 3s
-	setTimeout(complete, 3000);
-	//prevent default form submisson
-	return false;
-})
-	
+	tempMark.setPopup(new AnimatedPopup({ offset: 25, openingAnimation: {duration: 1000, easing: 'easeOutElastic'}, closingAnimation: { duration: 200, easing: 'easeInBack' } }).setHTML(editContent));
+		
 	//.setHTML(editContent));
-	
 	
 		    // $('#markery').unbind('click');
 		     //$("#markery").click(colorStart); 
