44f5cff60b47322ddd9e14794e7bee1614e1e74a
M <m@nfnth.com>
2022-09-13 15:44:14 -0700

    Update card.js

--- res/app/card.js
+++ res/app/card.js
@@ -2,8 +2,9 @@
 var suit = ['heart','diamond','club','spade']; var num = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 var cardSwitch = false;
 
-var cardCode = '<div class="flip-card cardy"><div class="flip-card-inner z-depth-1" style="cursor:pointer;"><div class="flip-card-front" onclick="flipCard();" style="z-index:1;"><img  src="XXXX" style="width:48px;height:48px;"></div><div class="flip-card-back" style="transform: rotateY(180deg);"><div id="tactb" style="opacity:0.75; display: flex;align-items: center;position: absolute;top: 4px;left: 4px;"><span id="tact1" style="font-size: 24px;margin-right: 8px;font-weight: bold;"></span><img style="cursor:pointer;" id="tacta" width="24" height="24" /></div><div id="tact2" style="opacity:0.75; display: flex;align-items: center;position: absolute;bottom: 4px;right: 4px;"></div></div></div>';
+var cardCode = '<div class="flip-card cardy"><div class="flip-card-inner z-depth-1" style="cursor:pointer;border-radius:12px;"><div class="flip-card-front" onclick="flipCard();" style="z-index:1;border-radius:12px;"><img  src="XXXX" style="width:48px;height:48px;"></div><div class="flip-card-back" style="transform: rotateY(180deg);border-radius:12px;"><div id="tactb" style="opacity:0.75; display: flex;align-items: center;position: absolute;top: 4px;left: 4px;"><span id="tact1" style="font-size: 24px;margin-right: 8px;font-weight: bold;"></span><img style="cursor:pointer;" id="tacta" width="24" height="24" /></div><div id="tact2" style="opacity:0.75; display: flex;align-items: center;position: absolute;bottom: 4px;right: 4px;"></div></div></div>';
 
+var cardOwn = '<div class="flip-card cardy" onclick="flipQR(YYYY);"><div class="flip-card-inner z-depth-1" style="cursor:pointer;border-radius:12px;"><div class="flip-card-front"  style="z-index:1;border-radius:12px;"><img  src="XXXX" style="width:48px;height:48px;"></div><div class="flip-card-back" style="transform: rotateY(180deg);border-radius:12px;"><div id="tactb" style="opacity:0.75; display: flex;align-items: center;position: absolute;top: 4px;left: 4px;"><span id="tactx" style="font-size: 24px;margin-right: 8px;font-weight: bold;"></span><img style="cursor:pointer;" id="tacta" width="24" height="24" /></div><div id="tact2" style="opacity:0.75; display: flex;align-items: center;position: absolute;bottom: 4px;right: 4px;"></div></div></div>';
 
 function flipCard() { var mySuit = Math.floor(Math.random() * (4 - 1 + 1)); var myNum = Math.floor(Math.random() * (13 - 1 + 1)); 
 	$("#tacta").attr("src","res/img/card/" + suit[mySuit] + ".png");$("#tact1").html(num[myNum]);
@@ -11,11 +12,18 @@ function flipCard() { var mySuit = Math.floor(Math.random() * (4 - 1 + 1)); var 
 		      $("#tact2").html($("#tactb").html());
 		     $('.flip-card-inner').css("transform","rotateY(180deg)");  }
 
-
-function flipQR(i) { $("#tact1").html(domains[i].owner.wallet); }
+function flipQR(i) { 
+	var options = {
+		text: domains[i].owner.wallet
+	};
+	
+	// Create QRCode Object
+	new QRCode(document.getElementById("tactx"), options);
+$('.flip-card-inner').css("transform","rotateY(180deg)"); 
+}
 
 function makeCard() { var leaf = Math.floor(Math.random() * (16 - 1 + 1)) + 1; var leafSrc = "res/img/leaf/leaf" + leaf.toString() + ".png"; 
                    return cardCode.replace('XXXX',leafSrc); }
 
 function makeCardOwner(i) { var profileSrc = 'wallet/' + domains[i].owner.wallet + '/profile'; 
-                   return cardCode.replace('XXXX',profileSrc); }
+                   return cardOwn.replace('XXXX',profileSrc).replace('YYYY',i); }
