ec8b87657bd4a126a5d93220fda871fabfa14eee
M <m@nfnth.com>
2023-08-03 00:10:31 +0000

    Delete res/os directory

--- res/os/arch/ocur.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/openrc-run
-
-#mkdir /run/weston
-command=export XDG_RUNTIME_DIR=/tmp/ && /usr/bin/weston
--- res/os/arch/ur.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /mnt/data
-mkdir /mnt/data/domain
-mkdir /mnt/data/wallet
-
-python site.py
